669eb745aeb92c0651d2b04b9567e0864ceb9d6b
Leo <cni586@aucklanduni.ac.nz>
2025-02-02 16:56:16 +1300

    Improve tag removal accessibility and update image upload button icon

--- frontend/src/routes/publish/+page.svelte
+++ frontend/src/routes/publish/+page.svelte
@@ -666,15 +666,18 @@
                       </svg>
                       {tag}
                     </span>
-                    <button
+                    <div
                       class="remove-button"
                       on:click|stopPropagation={() => removeTag(tag)}
+                      role="button"
+                      tabindex="0"
+                      on:keydown={(e) => e.key === 'Enter' && removeTag(tag)}
                       aria-label={`删除标签 ${tag}`}
                     >
                       <svg class="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                       </svg>
-                    </button>
+                    </div>
                   </div>
                 {/each}
               </div>
@@ -758,13 +761,9 @@
             title="插入图片"
             aria-label="插入图片"
           >
-            {#if imageUploading}
-              <div class="w-5 h-5 border-2 border-lime-500 border-t-transparent rounded-full animate-spin"></div>
-            {:else}
-              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-              </svg>
-            {/if}
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd" />
+            </svg>
           </button>
           <button
             class="toolbar-button"
